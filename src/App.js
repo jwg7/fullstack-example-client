789d9f638fc6a2c8237f721aa01fb0957a56a1dd
JG <105654452+jwg7@users.noreply.github.com>
2022-11-14 18:13:14 -0600

    commit 1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+
+const urlEndpoint = "http://localhost:4000";
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const result = await fetch(`${urlEndpoint}/blogs/all`);
+      const fetchedBlogs = await result.json();
+      console.log(fetchedBlogs);
+      setBlogs(fetchedBlogs.blogs);
+    };
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {blogs.map((blog) => {
+          return <div>{blog.title}</div>;
+        })}
+      </header>
+    </div>
+  );
+}
+
+export default App;
